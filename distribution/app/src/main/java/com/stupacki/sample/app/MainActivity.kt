90647fa2069a1b6e3b6da9fb3342ae8663c8db03
Benny Schneider <benny.schneider@rewe-digital.com>
2019-10-25 11:02:04 +0100

    Extract navigation setup and remove dummy tests

--- distribution/app/src/main/java/com/stupacki/sample/app/MainActivity.kt
+++ distribution/app/src/main/java/com/stupacki/sample/app/MainActivity.kt
@@ -1,19 +1,25 @@
 package com.stupacki.sample.app
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.stupacki.sample.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        setNavigation()
+    }
+
+    private fun setNavigation() {
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
