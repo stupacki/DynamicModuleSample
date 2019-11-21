fcdf66df126b1630f120c87dc59d204d126b6da6
Benny Schneider <benny.schneider@rewe-digital.com>
2019-11-21 10:24:27 +0100

    Add navigation options to main navigation

--- common/navigation/src/main/java/com/stupacki/sample/app/navigation/main/MainNavigation.kt
+++ common/navigation/src/main/java/com/stupacki/sample/app/navigation/main/MainNavigation.kt
@@ -1,7 +1,19 @@
 package com.stupacki.sample.app.navigation.main
 
 import androidx.navigation.NavController
+import com.stupacki.sample.app.navigation.R
 
 class MainNavigation(private val navController: NavController) {
 
+    fun navigateToHomeFeature() {
+        navController.navigate(R.id.toHomeFeature)
+    }
+
+    fun navigateToDashboardFeature() {
+        navController.navigate(R.id.toHomeFeature)
+    }
+
+    fun navigateToNotificationsFeature() {
+        navController.navigate(R.id.toHomeFeature)
+    }
 }
