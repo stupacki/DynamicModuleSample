644ed3e8e0ae5bf7fdfffbd955280aa9593d1898
Benny Schneider <benny.schneider@rewe-digital.com>
2019-11-21 16:36:58 +0100

    Fix notifications injection

--- domain/notifications/src/main/java/com/stupacki/sample/app/notifications/view/NotificationsFragment.kt
+++ domain/notifications/src/main/java/com/stupacki/sample/app/notifications/view/NotificationsFragment.kt
@@ -6,18 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.navigation.NavController
-import androidx.navigation.fragment.findNavController
-import com.stupacki.sample.app.core.CoreApplication
 import com.stupacki.sample.app.navigation.notifications.NotificationsNavigation
 import com.stupacki.sample.app.notifications.R
+import com.stupacki.sample.app.notifications.injection.NotificationsComponent
 import com.stupacki.sample.app.notifications.viewmodel.NotificationsViewModel
 import kotlinx.android.synthetic.main.fragment_notifications.*
 import org.rewedigital.katana.androidx.viewmodel.viewModel
 
 class NotificationsFragment : Fragment() {
 
-    private val component by lazy { CoreApplication.coreComponent }
+    private val component by lazy { NotificationsComponent() }
     private val viewModel by component.viewModel<NotificationsViewModel>(this)
     private val notificationsNavigation by component.inject<NotificationsNavigation>()
 
